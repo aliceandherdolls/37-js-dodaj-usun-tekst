4628b3c33664bac1ea29bfb298e77bcf0d196c14
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-18 15:36:53 +0200

    dodaj

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let button = document.querySelector('dodaj');
+
+
+dodaj.onclick = function () {
+    document.getElementById("paragraph").innerText = "To jest świetny opis";
+
+}
+
+let button2 = document.querySelector('usuń');
+
+usun.onclick = function (){
+    document.getElementById("paragraph").innerText= "";
+
+}
+
+
+
+
+
